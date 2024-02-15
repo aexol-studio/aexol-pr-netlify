ad38f9dac4dea6fa23eedc3dbbe4e965327c3d76
Tomek Poniatowicz <52368498+tomekponiatowicz@users.noreply.github.com>
2024-02-15 17:36:34 +0100

    fix

--- components/Header.js
+++ components/Header.js
@@ -2,24 +2,13 @@ export default function Header({ title }) {
   return (
     <>
       <h1 className="title">{title}</h1>
-      <p>
-        Hi 👋! This template gives you a{" "}
-        <a href="https://nextjs.org/">Next.js</a> app with the scaffolding for{" "}
-        <a href="https://www.netlify.com/products/functions/">
-          Netlify Functions
-        </a>
-        , <a href="https://www.netlify.com/products/forms/">Forms</a>, and{" "}
-        <a href="https://docs.netlify.com/routing/redirects/">Redirects</a>. Our
-        aim was to give you the code you would need to hit the ground running
-        with a few fun features.
-      </p>
+      <p>Hey there 👋!</p>
 
       <p>
-        You can find the code for this project on GitHub at{" "}
-        <a href="https://github.com/netlify-templates/nextjs-toolbox">
-          https://github.com/netlify-templates/nextjs-toolbox
-        </a>
-        ! Happy coding!
+        We're <a href="https://aexol.com">Aexol</a>, a technology-first
+        company's best friend. We love building mobile apps, web apps, SaaS
+        tools, open-source & blazingly fast ecommerece solutions based on
+        Vendure!
       </p>
     </>
   );
